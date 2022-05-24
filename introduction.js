c1ce4aa0383b1bd9b5b2e6b0fe1eb9dd679e07b9
belausabaev <b.usabaev@khm.de>
2022-05-24 11:41:06 +0200

    update new info text

--- introduction.js
+++ introduction.js
@@ -7,10 +7,12 @@ const tour = new Shepherd.Tour({
   },
 });
 
+
+
 tour.addStep({
   title: "Hi there!",
   text: `This is a virtual THEREMIN application.\
-  Let's have a look around. This application has several interactive sounds. The 'THEREMIN' sound, a 'SAMPLER' Theremin sound, the more atmospheric 'GRANULAR SYNTHESIS' sound and a range of prerendered SOUND EFFECTS.`,
+  Let's have a look around. This application has video, several interactive sounds and film music.`,
   buttons: [
     {
       action() {
@@ -30,10 +32,9 @@ tour.addStep({
 });
 
 tour.addStep({
-  title: "INTERACTIVE SOUNDS",
-  text: `The THEREMIN responds to your hand movements, so make sure to allow access to your webcam. To be able to hear anything, you will have to turn on the sound with the speaker
- button on the left and activate one of the interactive sounds (THEREMIN/SAMPLER/GRANULAR SYNTHESIS) or the film music (M and P).`,
-  attachTo: { element: ".ui-theremin", on: "top" },
+  title: "WEB THEREMIN",
+  text: `To start the web theremin activate the sound button in the upper left corner of the page.`,
+  attachTo: { element: ".SpeakerContainer", on: "right" },
   buttons: [
     {
       action() {
@@ -76,8 +77,8 @@ tour.addStep({
 */
 tour.addStep({
   title: "INTERACTIVE SOUNDS",
-  text: `Move your left hand up and down to change the volume of THEREMIN and SAMPLER. Move your right hand through space to change the pitch.`,
-  attachTo: { element: ".ui-granular", on: "top" },
+  text: `You can choose THEREMIN or SAMPLER to play sounds by hand movement. Activate and deactivate one or both of the sounds to play the sounds by hand movement.`,
+  attachTo: { element: ".ui-interactive-buttons", on: "top" },
   buttons: [
     {
       action() {
@@ -98,52 +99,8 @@ tour.addStep({
 
 tour.addStep({
   title: "INTERACTIVE SOUNDS",
-  text: `To use the SOUND EFFECTS activate the button M and P on the right side, in the video timeline, and move your left hand up and down.`,
-  attachTo: { element: ".ui-granular", on: "top" },
-  buttons: [
-    {
-      action() {
-        return this.back();
-      },
-      classes: "shepherd-button-secondary",
-      text: "Back",
-    },
-    {
-      action() {
-        return this.next();
-      },
-      text: "Next",
-    },
-  ],
-  id: "creating",
-});
-
-tour.addStep({
-  title: "INTERACTIVE SOUNDS",
-  text: `Activate the film music (M and P) on the left side, activate GRANULAR SYNTHESIS and use the sliders GRAIN SIZE and PLAYBACK RATE to hear how the parameters affect the sound. To use GRANULAR SYNTHESIS interactively, activate the checkboxes GRAIN SIZE or GRAIN DELAY, or both. Moving your right hand up and down affects the GRAIN DELAY, moving the right hand left and right affects the GRAIN SIZE. Moving your left hand up and down changes the volume.`,
-  attachTo: { element: ".ui-graindelay", on: "top" },
-  buttons: [
-    {
-      action() {
-        return this.back();
-      },
-      classes: "shepherd-button-secondary",
-      text: "Back",
-    },
-    {
-      action() {
-        return this.next();
-      },
-      text: "Next",
-    },
-  ],
-  id: "creating",
-});
-
-tour.addStep({
-  title: "INTERACTIVE SOUNDS",
-  text: `You can activate several interactive sounds at once and try out different hand movements.`,
-  attachTo: { element: ".ui-graindelay", on: "top" },
+  text: `With the left hand you control the volume of the sounds, with right hand tge pitch of the sounds.`,
+  attachTo: { element: ".ui-interactive-buttons", on: "top" },
   buttons: [
     {
       action() {
@@ -164,8 +121,209 @@ tour.addStep({
 
 tour.addStep({
   title: "FILM MUSIC",
-  text: `To better understand the value of film music, you can toggle on and off a precomposed piece of film music for the film scene. To hear the individual SCENE compositions, click a scene button. To listen to the whole composition, click the M or P buttons. You can toggle M and P on and off to hear the difference between leading and accompanying melodies.`,
-  attachTo: { element: ".ui-music", on: "top" },
+  text: `To hear the film music composed for this film scene activate the button with the sound grafik/waveform.`,
+  attachTo: { element: ".ui-filmsounds-melody", on: "top" },
+  buttons: [
+    {
+      action() {
+        return this.back();
+      },
+      classes: "shepherd-button-secondary",
+      text: "Back",
+    },
+    {
+      action() {
+        return this.next();
+      },
+      text: "Next",
+    },
+  ],
+  id: "creating",
+});
+
+tour.addStep({
+  title: "FILM MUSIC",
+  text: `This activates the main film melody.`,
+  attachTo: { element: ".ui-filmsounds-melody", on: "left" },
+  buttons: [
+    {
+      action() {
+        return this.back();
+      },
+      classes: "shepherd-button-secondary",
+      text: "Back",
+    },
+    {
+      action() {
+        return this.next();
+      },
+      text: "Next",
+    },
+  ],
+  id: "creating",
+});
+
+
+tour.addStep({
+  title: "FILM MUSIC",
+  text: `This activates the accompanying melody.`,
+  attachTo: { element: ".ui-filmsounds-polyphone", on: "left" },
+  buttons: [
+    {
+      action() {
+        return this.back();
+      },
+      classes: "shepherd-button-secondary",
+      text: "Back",
+    },
+    {
+      action() {
+        return this.next();
+      },
+      text: "Next",
+    },
+  ],
+  id: "creating",
+});
+
+tour.addStep({
+  title: "FILM MUSIC",
+  text: `You can choose between different scenes of the video using the labeled buttons on top of the time line.
+  When you choose a scene by label, the video scene and its sound will loop, until you deactivate the label.`,
+  attachTo: { element: ".ui-filmsounds", on: "top" },
+  buttons: [
+    {
+      action() {
+        return this.back();
+      },
+      classes: "shepherd-button-secondary",
+      text: "Back",
+    },
+    {
+      action() {
+        return this.next();
+      },
+      text: "Next",
+    },
+  ],
+  id: "creating",
+});
+
+tour.addStep({
+  title: "VIDEO CONTROLS",
+  text: `Use the slider of the video to choose a portion of the video, that you want to watch and listen to. You can also start and pause the video. The film sound will pause too.`,
+  attachTo: {element: ".ui-video", on: "top"},
+  buttons: [
+    {
+      action() {
+        return this.back();
+      },
+      classes: "shepherd-button-secondary",
+      text: "Back",
+    },
+    {
+      action() {
+        return this.next();
+      },
+      text: "Next",
+    },
+  ],
+  id: "creating",
+});
+
+tour.addStep({
+  title: "SOUND EFFECTS",
+  text: `You can choose between three sound effects, and fade between them using hand movement. For this activate the sound effects button.`,
+  attachTo: {element: ".btn-effect", on: "left"},
+  buttons: [
+    {
+      action() {
+        return this.back();
+      },
+      classes: "shepherd-button-secondary",
+      text: "Back",
+    },
+    {
+      action() {
+        return this.next();
+      },
+      text: "Next",
+    },
+  ],
+  id: "creating",
+});
+
+tour.addStep({
+  title: "GRANULAR SYNTHESIS",
+  text: `You can apply a granular feedback delay effect to the film music. For this activate the granular synthesis button and choose one ot both of the granular effect buttons below.`,
+  attachTo: {element: ".btn-granular", on: "left"},
+  buttons: [
+    {
+      action() {
+        return this.back();
+      },
+      classes: "shepherd-button-secondary",
+      text: "Back",
+    },
+    {
+      action() {
+        return this.next();
+      },
+      text: "Done",
+    },
+  ],
+  id: "creating",
+});
+
+tour.addStep({
+  title: "GRANULAR SYNTHESIS",
+  text: `the applied effect is controlled by hand movement.
+  You control the sound effects by moving your right hand left and right`,
+  attachTo: {element: ".btn-gran-size", on: "left"},
+  buttons: [
+    {
+      action() {
+        return this.back();
+      },
+      classes: "shepherd-button-secondary",
+      text: "Back",
+    },
+    {
+      action() {
+        return this.next();
+      },
+      text: "Done",
+    },
+  ],
+  id: "creating",
+});
+
+tour.addStep({
+  title: "GRANULAR DELAY EFFECT",
+  text: `or up and down`,
+  attachTo: {element: ".btn-gran-delay", on: "right"},
+  buttons: [
+    {
+      action() {
+        return this.back();
+      },
+      classes: "shepherd-button-secondary",
+      text: "Back",
+    },
+    {
+      action() {
+        return this.next();
+      },
+      text: "Done",
+    },
+  ],
+  id: "creating",
+});
+
+tour.addStep({
+  title: "GRANULAR SYNTHESIS",
+  text: `When you deactivate the effect buttons the effect will be removed from the music.`,
+  attachTo: {element: ".ui-interactive-effects-sett", on: "top"},
   buttons: [
     {
       action() {
@@ -187,7 +345,29 @@ tour.addStep({
 tour.addStep({
   title: "ATMOSPHERE FOLEY",
   text: `Aside from film music, atmospheric sound, also called FOLEYS, help with bringing a film scene to life. Click on the different sound sources to hear what a difference these sounds can make.`,
-  attachTo: { element: ".ui-foley", on: "top" },
+  attachTo: { element: ".ui-filmsounds-foley", on: "top" },
+  buttons: [
+    {
+      action() {
+        return this.back();
+      },
+      classes: "shepherd-button-secondary",
+      text: "Back",
+    },
+    {
+      action() {
+        return this.next();
+      },
+      text: "Next",
+    },
+  ],
+  id: "creating",
+});
+
+tour.addStep({
+  title: "HELP",
+  text: `You can always come back to this introduction tour by clicking the lightbulb button.`,
+  attachTo: { element: ".infoContainer", on: "right" },
   buttons: [
     {
       action() {
@@ -226,5 +406,3 @@ tour.addStep({
   ],
   id: "creating",
 });
-
-// tour.start();
